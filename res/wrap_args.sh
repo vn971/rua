3802f5dd7370b692702f3c9ada9b31814d46e236
Vasya Novikov <n1dr+cm3513git@ya.ru>
2018-10-30 17:35:33 +0300

    improve CLI names

--- res/wrap_args.sh
+++ res/wrap_args.sh
@@ -1,4 +1,10 @@
 wrap_args=()
 
 # Example: unsafe shared directories.
-# wrap_args=( --bind ~/.cargo ~/.cargo --bind ~/.rustup ~/.rustup )
+# wrap_args=(
+#     --bind ~/.cargo ~/.cargo
+#     --bind ~/.rustup ~/.rustup
+#     --bind ~/.ivy2 ~/.ivy2
+#     --bind ~/.m2 ~/.m2
+#     --bind ~/.coursier ~/.coursier
+# )

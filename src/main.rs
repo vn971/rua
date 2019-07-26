00c43ce71aec0981b5ad0a323e8742d54ce06980
Vasili Novikov <n1dr+cm3513git@ya.ru>
2019-07-27 00:27:10 +0200

    only make build-related actions when building
    For example, if you're root, there's no point to
    ever create ~/.config/rua/wrap_args.d/ and other directories

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,7 @@ use fs2::FileExt;
 use log::debug;
 use structopt::StructOpt;
 
-fn default_env(key: &str, value: &str) {
+fn set_env_if_not_set(key: &str, value: &str) {
 	if env::var_os(key).is_none() {
 		env::set_var(key, value);
 	}
@@ -79,7 +79,7 @@ fn overwrite_script(path: &Path, content: &[u8]) {
 }
 
 fn main() {
-	default_env("RUST_BACKTRACE", "1"); // if it wasn't set to "0" explicitly, set it to 1.
+	set_env_if_not_set("RUST_BACKTRACE", "1"); // if it wasn't set to "0" explicitly, set it to 1.
 	env_logger::Builder::from_env(Env::default().filter_or("LOG_LEVEL", "info"))
 		.format(|buf, record| {
 			writeln!(
@@ -96,27 +96,12 @@ fn main() {
 		env!("CARGO_PKG_NAME"),
 		env!("CARGO_PKG_VERSION")
 	);
+	let dirs = ProjectDirs::from("com.gitlab", "vn971", "rua")
+		.expect("Failed to determine XDG directories");
 	let config: CliArgs = CliArgs::from_args();
 	match config.action {
-		Action::Install { .. } | Action::Jailbuild { .. } => {
-			if users::get_current_uid() == 0 {
-				eprintln!("RUA should not be run as root.");
-				eprintln!("Also, makepkg will not allow you building from root anyway.");
-				exit(1)
-			}
-			if !Command::new("bwrap")
-				.args(&["--ro-bind", "/", "/", "true"])
-				.status()
-				.expect("bwrap binary not found. RUA uses bubblewrap for security isolation.")
-				.success()
-			{
-				eprintln!("Failed to run bwrap.");
-				eprintln!("A possible cause for this is if RUA itself is run in jail (docker, bwrap, firejail,..).");
-				eprintln!("If so, see https://github.com/vn971/rua/issues/8");
-				exit(4)
-			}
-		}
-		_ => {}
+		Action::Install { .. } | Action::Jailbuild { .. } => prepare_for_jailed_action(&dirs),
+		_ => {},
 	}
 	match config.color {
 		// see "colored" crate and referenced specs
@@ -149,42 +134,8 @@ fn main() {
 		"PKGEXT environment is set to an incompatible value. \
 		 Only *.tar and *.tar.xz are supported."
 	);
-	default_env("PKGEXT", ".pkg.tar.xz");
+	set_env_if_not_set("PKGEXT", ".pkg.tar.xz");
 
-	let dirs = ProjectDirs::from("com.gitlab", "vn971", "rua")
-		.expect("Failed to determine XDG directories");
-	std::fs::create_dir_all(dirs.cache_dir()).expect("Failed to create project cache directory");
-	rm_rf::force_remove_all(dirs.config_dir().join(".system"), true).ok();
-	std::fs::create_dir_all(dirs.config_dir().join(".system"))
-		.expect("Failed to create project config directory");
-	std::fs::create_dir_all(dirs.config_dir().join("wrap_args.d"))
-		.expect("Failed to create project config directory");
-	overwrite_file(
-		&dirs.config_dir().join(".system/seccomp-i686.bpf"),
-		include_bytes!("../res/seccomp-i686.bpf"),
-	);
-	overwrite_file(
-		&dirs.config_dir().join(".system/seccomp-x86_64.bpf"),
-		include_bytes!("../res/seccomp-x86_64.bpf"),
-	);
-	let seccomp_path = format!(
-		".system/seccomp-{}.bpf",
-		uname::uname()
-			.expect("Failed to get system architecture via uname")
-			.machine
-	);
-	default_env(
-		"RUA_SECCOMP_FILE",
-		dirs.config_dir().join(seccomp_path).to_str().unwrap(),
-	);
-	overwrite_script(
-		&dirs.config_dir().join(wrapped::WRAP_SCRIPT_PATH),
-		include_bytes!("../res/wrap.sh"),
-	);
-	ensure_script(
-		&dirs.config_dir().join(".system/wrap_args.sh.example"),
-		include_bytes!("../res/wrap_args.sh"),
-	);
 	let locked_file = File::open(dirs.config_dir()).expect("Failed to find config dir for locking");
 	locked_file.try_lock_exclusive().unwrap_or_else(|_| {
 		eprintln!("Another RUA instance already running.");
@@ -210,4 +161,55 @@ fn main() {
 			eprintln!("Finished checking pachage: {:?}", target);
 		}
 	};
+}
+
+fn prepare_for_jailed_action(dirs: &ProjectDirs) {
+	if users::get_current_uid() == 0 {
+		eprintln!("RUA does not allow building as root.");
+		eprintln!("Also, makepkg will not allow you building as root anyway.");
+		exit(1)
+	}
+	if !Command::new("bwrap")
+		.args(&["--ro-bind", "/", "/", "true"])
+		.status()
+		.expect("bwrap binary not found. RUA uses bubblewrap for security isolation.")
+		.success()
+	{
+		eprintln!("Failed to run bwrap.");
+		eprintln!("A possible cause for this is if RUA itself is run in jail (docker, bwrap, firejail,..).");
+		eprintln!("If so, see https://github.com/vn971/rua/issues/8");
+		exit(4)
+	}
+	std::fs::create_dir_all(dirs.cache_dir()).expect("Failed to create project cache directory");
+	rm_rf::force_remove_all(dirs.config_dir().join(".system"), true).ok();
+	std::fs::create_dir_all(dirs.config_dir().join(".system"))
+		.expect("Failed to create project config directory");
+	std::fs::create_dir_all(dirs.config_dir().join("wrap_args.d"))
+		.expect("Failed to create project config directory");
+	overwrite_file(
+		&dirs.config_dir().join(".system/seccomp-i686.bpf"),
+		include_bytes!("../res/seccomp-i686.bpf"),
+	);
+	overwrite_file(
+		&dirs.config_dir().join(".system/seccomp-x86_64.bpf"),
+		include_bytes!("../res/seccomp-x86_64.bpf"),
+	);
+	let seccomp_path = format!(
+		".system/seccomp-{}.bpf",
+		uname::uname()
+			.expect("Failed to get system architecture via uname")
+			.machine
+	);
+	set_env_if_not_set(
+		"RUA_SECCOMP_FILE",
+		dirs.config_dir().join(seccomp_path).to_str().unwrap(),
+	);
+	overwrite_script(
+		&dirs.config_dir().join(wrapped::WRAP_SCRIPT_PATH),
+		include_bytes!("../res/wrap.sh"),
+	);
+	ensure_script(
+		&dirs.config_dir().join(".system/wrap_args.sh.example"),
+		include_bytes!("../res/wrap_args.sh"),
+	);
 }

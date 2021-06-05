e0e253f85f6f572816b99418355e4696dcc16c4f
Vasili Novikov <n1dr+cm3513git@ya.ru>
2021-06-05 09:32:20 +0200

    Escape shell arguments whenever possible
    (Even if arguments are already checked anyway..)

--- src/pacman.rs
+++ src/pacman.rs
@@ -35,7 +35,7 @@ fn ensure_packages_installed(mut packages: Vec<(String, PathBuf)>, base_args: &[
 			list.sort_unstable();
 			eprintln!("Packages need to be installed:");
 			eprintln!(
-				"\n    pacman {} {}\n",
+				"\n    pacman {} -- {}\n",
 				base_args.join(" "),
 				list.iter()
 					.map(|p| terminal_util::escape_bash_arg(p)) // this is only printing. rua does not use bash to install packages
@@ -60,6 +60,7 @@ fn ensure_packages_installed(mut packages: Vec<(String, PathBuf)>, base_args: &[
 				let exit_status = Command::new(rua_environment::sudo_command())
 					.arg("pacman")
 					.args(base_args)
+					.arg("--")
 					.args(&list)
 					.status();
 				if exit_status.map(|c| c.success()).unwrap_or(false) {

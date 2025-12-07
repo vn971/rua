75c558e1226ac033507306a3f3a30b7067bdae7f
pgray <pgray@file.photo>
2025-12-07 13:12:19 -0800

    no unwrap

--- src/action_list.rs
+++ src/action_list.rs
@@ -2,7 +2,14 @@ use crate::alpm_wrapper::new_alpm_wrapper;
 
 pub fn list_real() {
 	let alpm = new_alpm_wrapper();
-	for (name, version) in alpm.get_non_pacman_packages().unwrap().iter() {
+	let packages = match alpm.get_non_pacman_packages() {
+		Ok(packages) => packages,
+		Err(err) => {
+			eprintln!("Error: {}", err);
+			return;
+		}
+	};
+	for (name, version) in packages.iter() {
 		println!("{} {}", name, version);
 	}
 }
